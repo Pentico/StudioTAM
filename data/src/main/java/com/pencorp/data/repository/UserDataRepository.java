8ec9b033f954ed5c906c164ef64aa70ab358671d
Pentico <morulaneat@gmail.com>
2017-02-03 16:09:38 +0200

    Work in data and domain modules...Continues

--- data/src/main/java/com/pencorp/data/repository/UserDataRepository.java
+++ data/src/main/java/com/pencorp/data/repository/UserDataRepository.java
@@ -4,5 +4,33 @@ package com.pencorp.data.repository;
  * Created by Tuane on 3/02/17.
  */
 
+import com.pencorp.data.repository.datasource.UserDataStore;
+import com.pencorp.data.repository.datasource.UserDataStoreFactory;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+import rx.Observable;
+
+/**
+ * {@link UserRepository} for retrieving user data.
+ */
+@Singleton
 public class UserDataRepository {
+
+    private final UserDataStoreFactory userDataStoreFactory;
+    private final UserEntityDataMapper userEntityDataMapper;
+
+    @Inject
+    public UserDataRepository(UserDataStoreFactory dataStoreFactory,
+                              UserEntityDataMapper userEntityDataMapper){
+        this.userDataStoreFactory = dataStoreFactory;
+        this.userEntityDataMapper = userEntityDataMapper;
+    }
+
+    @Override public Observable<User> user(int userId) {
+        final UserDataStore userDataStore = this.userDataStoreFactory.create(userId);
+        return userDataStore.userEntityDetails(userId).map(this.userEntityDataMapper::transform);
+    }
 }
+
