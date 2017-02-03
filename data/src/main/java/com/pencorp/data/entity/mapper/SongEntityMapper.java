dba8cd472519ec10e1dc928aae6073188d8ecc39
Pentico <morulaneat@gmail.com>
2017-02-03 14:59:11 +0200

    Added SongEntiityMapper and UserEntityMapper ... Todo mas tarder

---
+++ data/src/main/java/com/pencorp/data/entity/mapper/SongEntityMapper.java
@@ -0,0 +1,8 @@
+package com.pencorp.data.entity.mapper;
+
+/**
+ * Created by Tuane on 3/02/17.
+ */
+
+public class SongEntityMapper {
+}
---
+++ data/src/main/java/com/pencorp/data/entity/mapper/UserEntityDataMapper.java
@@ -0,0 +1,21 @@
+/*
+package com.pencorp.data.entity.mapper;
+
+import javax.inject.Singleton;
+
+*/
+/**
+ * Created by Tuane on 3/02/17.
+ *//*
+
+
+*/
+/**
+ * Mapper class used to transform {@link UserEntity} (in the data layer) to {@link User} in the
+ * domain layer.
+ *//*
+
+@Singleton
+public class UserEntityDataMapper {
+}
+*/
