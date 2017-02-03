3046079d98e7e05d7515dc178edfb44dd8894d54
Pentico <morulaneat@gmail.com>
2017-02-03 13:34:31 +0200

    Added Presentaton exception folder Added ErrorMessageFactoryClass Moved MainActivity

--- Presentation/src/main/java/com/pencorp/studiotam/View/activity/MainActivity.java
+++ Presentation/src/main/java/com/pencorp/studiotam/View/activity/MainActivity.java
@@ -1,11 +1,12 @@
-package com.pencorp.studiotam;
+package com.pencorp.studiotam.View.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.pencorp.studiotam.R;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
