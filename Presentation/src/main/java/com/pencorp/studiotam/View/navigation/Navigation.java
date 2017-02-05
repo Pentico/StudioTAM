701c9833cadf34f4475023859d99572a7bf096fe
Pentico <morulaneat@gmail.com>
2017-02-05 20:36:13 +0200

    Navigation for moving through the app..

---
+++ Presentation/src/main/java/com/pencorp/studiotam/View/navigation/Navigation.java
@@ -0,0 +1,22 @@
+package com.pencorp.studiotam.View.navigation;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Created by Tuane on 5/02/17.
+ */
+
+
+/**
+ * Class used to navigate through the application.
+ */
+@Singleton
+public class Navigation {
+
+    @Inject
+    public Navigation() {
+        //empty
+    }
+
+}
