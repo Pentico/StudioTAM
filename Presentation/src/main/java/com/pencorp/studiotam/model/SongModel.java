1d555a2526a302048ec5394fbae40ae86c31ea82
Pentico <morulaneat@gmail.com>
2017-02-09 21:00:05 +0200

    cont... Tired...

--- Presentation/src/main/java/com/pencorp/studiotam/model/SongModel.java
+++ Presentation/src/main/java/com/pencorp/studiotam/model/SongModel.java
@@ -1,8 +1,19 @@
 package com.pencorp.studiotam.model;
 
+import com.pencorp.studiotam.internal.di.PerActivity;
+
+import javax.inject.Inject;
+
 /**
  * Created by Tuane on 9/02/17.
+ * Class that represents a song in the presentation layer.
  */
-
 public class SongModel {
+
+    private  final long songId;
+
+    public SongModel(long songId){
+        this.songId = songId;
+    }
+
 }
