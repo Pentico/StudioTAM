4564d0ffc8803246a76cef7c141d0e1a25c2679f
Pentico <morulaneat@gmail.com>
2017-02-10 17:51:12 +0200

    SongsAdapter

---
+++ Presentation/src/main/java/com/pencorp/studiotam/View/adapter/SongsLayoutManager.java
@@ -0,0 +1,17 @@
+package com.pencorp.studiotam.View.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+
+/**
+ * Created by Tuane on 10/02/17.
+ */
+
+/**
+ * Layout manager to position items inside a {@link android.support.v7.widget.RecyclerView}.
+ */
+public class SongsLayoutManager extends LinearLayoutManager{
+    public SongsLayoutManager(Context context) {
+        super(context);
+    }
+}
