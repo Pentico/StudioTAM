c9d3b1d338d131ba2f7aa409564b89010b0aa1d9
Pentico <morulaneat@gmail.com>
2017-02-10 18:18:55 +0200

    SongDetailsActivity

---
+++ Presentation/src/main/java/com/pencorp/studiotam/View/activity/SongDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.pencorp.studiotam.View.activity;
+
+import com.pencorp.studiotam.internal.di.HasComponent;
+import com.pencorp.studiotam.internal.di.components.SongComponent;
+
+/**
+ * Created by Tuane on 10/02/17.
+ */
+
+public class SongDetailsActivity extends BaseActivity implements HasComponent<SongComponent> {
+
+    private static final String INTENT_EXTRA_PARAM_SONG_ID = "org.studiotam.INTENT_PARAM_SONG_ID";
+    private static final String INTENT_EXTRA_STATE_PARAM
+
+    @Override
+    public SongComponent getComponent() {
+        return null;
+    }
+}
