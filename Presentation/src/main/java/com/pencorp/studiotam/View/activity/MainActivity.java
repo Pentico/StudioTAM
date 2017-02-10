4cbe2517d84acf5c0673e79967eafad5cb53e3f2
Pentico <morulaneat@gmail.com>
2017-02-10 17:54:35 +0200

    MainActivity

--- Presentation/src/main/java/com/pencorp/studiotam/View/activity/MainActivity.java
+++ Presentation/src/main/java/com/pencorp/studiotam/View/activity/MainActivity.java
@@ -2,15 +2,30 @@ package com.pencorp.studiotam.View.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.pencorp.studiotam.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 
 public class MainActivity extends BaseActivity {
+
+    @Bind(R.id.btn_LoadData)
+    Button btn_LoadData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    /**
+     * Goes to the song list screen
+     */
+    void navigateToSongList(){
+
     }
 }
