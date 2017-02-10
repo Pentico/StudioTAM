543333ebe1b9f27fdccfb6da7c9c8514f14aa189
Pentico <morulaneat@gmail.com>
2017-02-10 17:11:24 +0200

    dependencies

---
+++ Presentation/src/main/java/com/pencorp/studiotam/View/adapter/SongAdapter.java
@@ -0,0 +1,10 @@
+package com.pencorp.studiotam.View.adapter;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by Tuane on 10/02/17.
+ */
+
+public class SongAdapter extends RecyclerView {
+}
