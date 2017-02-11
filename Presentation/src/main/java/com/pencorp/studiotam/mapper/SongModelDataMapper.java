752d54c0bb0100d6d679c47fa47059464416c619
Pentico <morulaneat@gmail.com>
2017-02-11 11:37:07 +0200

    SongModelDataMapper

--- Presentation/src/main/java/com/pencorp/studiotam/mapper/SongModelDataMapper.java
+++ Presentation/src/main/java/com/pencorp/studiotam/mapper/SongModelDataMapper.java
@@ -4,12 +4,21 @@ import com.pencorp.domain.Song;
 import com.pencorp.studiotam.internal.di.PerActivity;
 import com.pencorp.studiotam.model.SongModel;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
 import javax.inject.Inject;
 
 /**
  * Created by Tuane on 9/02/17.
  */
 
+/**
+ * Mapper class used to transform {@link Song} (in the domain layer) to
+ * {@link SongModel} in the
+ * presentation layer.
+ */
 @PerActivity
 public class SongModelDataMapper {
 
@@ -30,5 +39,26 @@ public class SongModelDataMapper {
         songModel.setTrack_no(song.getTrack_no());
         songModel.setDuration_ms(song.getDuration_ms());
         return songModel;
+    }
+
+    /**
+     * Transform a Collection of {@link Song} into a Collection of {@link SongModel}.
+     *
+     * @param songCollection Objects to be transformed.
+     * @return List of {@link SongModel}.
+     */
+    public Collection<SongModel> transform(Collection<Song> songCollection) {
+        Collection<SongModel> songModelsCollection;
+
+        if (songCollection != null && !songCollection.isEmpty()) {
+            songModelsCollection = new ArrayList<>();
+            for (Song song : songCollection) {
+                songModelsCollection.add(transform(song));
+            }
+        } else {
+            songModelsCollection = Collections.emptyList();
+        }
+
+        return songModelsCollection;
     }
 }
