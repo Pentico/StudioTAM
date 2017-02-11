672a13af9f1433a5a022cdebf2f314a517274b1e
Pentico <morulaneat@gmail.com>
2017-02-11 09:12:07 +0200

    SongDetailsFragment

--- Presentation/src/main/java/com/pencorp/studiotam/View/fragment/SongDetailsFragment.java
+++ Presentation/src/main/java/com/pencorp/studiotam/View/fragment/SongDetailsFragment.java
@@ -1,9 +1,20 @@
 package com.pencorp.studiotam.View.fragment;
 
 import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
+import com.pencorp.studiotam.R;
 import com.pencorp.studiotam.View.SongDetailsView;
+import com.pencorp.studiotam.internal.di.components.SongComponent;
 import com.pencorp.studiotam.model.SongModel;
+import com.pencorp.studiotam.presenter.SongDetailsPresenter;
+
+import javax.inject.Inject;
+
+import butterknife.ButterKnife;
 
 /**
  * Created by Tuane on 11/02/17.
@@ -13,6 +24,37 @@ import com.pencorp.studiotam.model.SongModel;
  * Fragment that shows details of a certian song.
  */
 public class SongDetailsFragment extends BaseFragment implements SongDetailsView {
+
+    @Inject
+    SongDetailsPresenter songDetailsPresenter;
+
+    public SongDetailsFragment() {
+        setRetainInstance(true);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.getComponent(SongComponent.class).inject(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                            Bundle savedInstanceState) {
+        final View fragmentView = inflater.inflate(R.layout.fragment_song_details, container, false);
+        ButterKnife.bind(this, fragmentView);
+        return fragmentView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.songDetailsPresenter.setView(this);
+        if(savedInstanceState == null){
+            this.loadSongDetails();
+        }
+    }
+
     @Override
     public void renderSong(SongModel song) {
 
