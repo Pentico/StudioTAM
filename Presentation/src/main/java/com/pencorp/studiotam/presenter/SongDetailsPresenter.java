87936b5a5f308f5236d66a50615c0540ce1fd8ee
Pentico <morulaneat@gmail.com>
2017-02-11 10:41:59 +0200

    SongDetailsPresenter

--- Presentation/src/main/java/com/pencorp/studiotam/presenter/SongDetailsPresenter.java
+++ Presentation/src/main/java/com/pencorp/studiotam/presenter/SongDetailsPresenter.java
@@ -1,6 +1,14 @@
 package com.pencorp.studiotam.presenter;
 
+import android.support.annotation.NonNull;
+
+import com.pencorp.domain.interactor.UseCase;
+import com.pencorp.studiotam.View.SongDetailsView;
 import com.pencorp.studiotam.internal.di.PerActivity;
+import com.pencorp.studiotam.mapper.SongModelDataMapper;
+
+import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
  * Created by Tuane on 10/02/17.
@@ -14,6 +22,21 @@ import com.pencorp.studiotam.internal.di.PerActivity;
 @PerActivity
 public class SongDetailsPresenter implements Presenter {
 
+    private SongDetailsView songDetailsView;
+
+    private final UseCase getSongDetailsUseCase;
+    private final SongModelDataMapper songModelDataMapper;
+
+    @Inject
+    public SongDetailsPresenter(@Named("songDetails") UseCase getSongDetailsUseCase,
+                                SongModelDataMapper songModelDataMapper) {
+        this.getSongDetailsUseCase = getSongDetailsUseCase;
+        this.songModelDataMapper = songModelDataMapper;
+    }
+
+    public void setView(@NonNull SongDetailsView view) {
+        this.songDetailsView = view;
+    }
 
     @Override
     public void resume() {
@@ -27,6 +50,25 @@ public class SongDetailsPresenter implements Presenter {
 
     @Override
     public void destroy() {
-
+        this.getSongDetailsUseCase.unsubscribe();
+        this.songDetailsView = null;
     }
+
+    /**
+     * Initialize the presenter by start retrieving song details.
+     */
+    public void initialize() {
+        this.loadSongDetails();
+    }
+
+    /**
+     * Loads song details
+     */
+    private void loadSongDetails() {
+        this.hideViewRetry();
+        this.showViewLoading();
+        this.getSongDetails();
+    }
+
+    
 }
