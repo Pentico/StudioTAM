a5f4455ce2761de3cf5a9d83a6f37d630d125496
Pentico <morulaneat@gmail.com>
2017-02-11 12:48:17 +0200

    Navigation

--- Presentation/src/main/java/com/pencorp/studiotam/View/navigation/Navigation.java
+++ Presentation/src/main/java/com/pencorp/studiotam/View/navigation/Navigation.java
@@ -1,5 +1,11 @@
 package com.pencorp.studiotam.View.navigation;
 
+import android.content.Context;
+import android.content.Intent;
+
+import com.pencorp.studiotam.View.activity.SongDetailsActivity;
+import com.pencorp.studiotam.View.activity.SongListActivity;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
@@ -17,6 +23,25 @@ public class Navigation {
     @Inject
     public Navigation() {
         //empty
+    }
+
+    /**
+     * Goes to the song list screen.
+     *
+     * @param context A Context needed to open the destiny activity.
+     */
+    public void navigatetToSongList(Context context) {
+        if (context != null) {
+            Intent intentToLaunch = SongListActivity.getCallingIntent(context);
+            context.startActivity(intentToLaunch);
+        }
+    }
+
+    public void navigateToSongDetails(Context context, long songId) {
+        if (context != null) {
+            Intent intentToLaunch = SongDetailsActivity.getCallingIntent(context, songId);
+            context.startActivity(intentToLaunch);
+        }
     }
 
 }
