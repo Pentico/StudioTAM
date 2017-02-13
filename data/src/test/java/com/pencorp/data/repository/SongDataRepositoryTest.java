c74f53277584b4c30da5561495808a4b8f1b3033
Pentico <morulaneat@gmail.com>
2017-02-13 23:26:21 +0200

    RepositoryErrorBundleTest continues in 4 hours ...

--- data/src/test/java/com/pencorp/data/repository/SongDataRepositoryTest.java
+++ data/src/test/java/com/pencorp/data/repository/SongDataRepositoryTest.java
@@ -1,10 +1,55 @@
 package com.pencorp.data.repository;
 
 import com.pencorp.data.ApplicationTestCase;
+import com.pencorp.data.entity.SongEntity;
+import com.pencorp.data.entity.mapper.SongEntityDataMapper;
+import com.pencorp.data.repository.datasource.SongData.SongDataStore;
+import com.pencorp.data.repository.datasource.SongData.SongDataStoreFactory;
+import com.pencorp.domain.Song;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.anyInt;
 
 /**
  * Created by Tuane on 9/02/17.
  */
 
 public class SongDataRepositoryTest extends ApplicationTestCase {
+
+    private static final long FAKE_SONG_ID = 586;
+
+    private SongDataRepository songDataRepository;
+
+    @Mock
+    private SongDataStoreFactory mockSongDataStoreFactory;
+    @Mock
+    private SongEntityDataMapper mockSongEntityDataMapper;
+    @Mock
+    private SongDataStore mockSongDataStore;
+    @Mock
+    private SongEntity mockSongEntity;
+    @Mock
+    private Song mockSong;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        songDataRepository = new SongDataRepository(mockSongEntityDataMapper,
+                mockSongDataStoreFactory);
+
+        given(mockSongDataStoreFactory.create(anyInt())).willReturn(mockSongDataStore);
+        given(mockSongDataStoreFactory.createDiskDataStore()).willReturn(mockSongDataStore);
+
+    }
+
+
 }
