cf697be223bfa6dd711a5e3508c18fd2165bb0c9
Pentico <morulaneat@gmail.com>
2017-02-13 12:51:33 +0200

    To find the bug, I have to test every piece of code...

--- data/src/test/java/com/pencorp/data/ApplicationTestCase.java
+++ data/src/test/java/com/pencorp/data/ApplicationTestCase.java
@@ -5,13 +5,13 @@ package com.pencorp.data;
  */
 
 import org.junit.runner.RunWith;
-import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
 /**
  * Base class for Robolectric data layer tests.
  * Inherit from this class to create a test.
  */
-@RunWith(RobolectricGradleTestRunner.class)
+@RunWith(RobolectricTestRunner.class)
 @Config(constants = BuildConfig.class, application = ApplicationStub.class, sdk = 21)
 public abstract class ApplicationTestCase {}
