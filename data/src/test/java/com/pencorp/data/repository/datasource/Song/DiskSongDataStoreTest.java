3587361a5c5c965442ddc6429ac6f5aff177b6df
Pentico <morulaneat@gmail.com>
2017-02-14 09:34:46 +0200

    DiskSongDataStoreTest

--- data/src/test/java/com/pencorp/data/repository/datasource/Song/DiskSongDataStoreTest.java
+++ data/src/test/java/com/pencorp/data/repository/datasource/Song/DiskSongDataStoreTest.java
@@ -2,8 +2,14 @@ package com.pencorp.data.repository.datasource.Song;
 
 import com.pencorp.data.ApplicationTestCase;
 import com.pencorp.data.cache.Song.SongCache;
+import com.pencorp.data.repository.datasource.SongData.DiskSongDataStore;
 
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 /**
  * Created by Tuane on 9/02/17.
@@ -13,8 +19,26 @@ public class DiskSongDataStoreTest extends ApplicationTestCase {
 
     private static final long FAKE_SONG_ID = 789;
 
-    private DiskSongDataStoreTest diskSongDataStoreTest;
+    private DiskSongDataStore diskSongDataStore;
 
     @Mock
     private SongCache mockSongCache;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        diskSongDataStore = new DiskSongDataStore(mockSongCache);
+    }
+
+    /**
+     * This should Fail Now cause I have implimented the Operation
+     */
+    @Test
+    public void testGetSongEntityListUnsupported() {
+        expectedException.expect(UnsupportedOperationException.class);
+        diskSongDataStore.songEntityList();
+    }
 }
