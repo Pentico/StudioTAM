7f1356ef12c9b9efd332911a3c71e6dc0f08e626
Pentico <morulaneat@gmail.com>
2017-02-15 22:27:33 +0200

    UserTesting ......

--- domain/src/test/java/com/pencorp/domain/UserTest.java
+++ domain/src/test/java/com/pencorp/domain/UserTest.java
@@ -3,7 +3,7 @@ package com.pencorp.domain;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
 /**
