bd1e8b862ec114f93e29bab5fac7c63b5915b85c
Pentico <morulaneat@gmail.com>
2017-02-16 11:13:43 +0200

    Problema with Presentation Domain for testiing

---
+++ Presentation/src/test/java/com/pencorp/studiotam/exception/ErrorMessageFactoryTest.java
@@ -0,0 +1,39 @@
+package com.pencorp.studiotam.exception;
+
+import android.test.AndroidTestCase;
+
+import com.pencorp.data.exception.NetworkConnectionException;
+import com.pencorp.data.exception.SongNotFoundException;
+import com.pencorp.studiotam.R;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by Tuane on 16/02/17.
+ */
+
+public class ErrorMessageFactoryTest extends AndroidTestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void testNetworkConnectinErrorMessage() {
+
+        String expectedMessage = getContext().getString(R.string.exception_message_no_connection);
+        String actualMessage = ErrorMessageFactory.create(getContext(),
+                new NetworkConnectionException());
+
+        assertThat(actualMessage, is(equalTo(expectedMessage)));
+    }
+
+    public void testUserNotFoundErrorMessage() {
+
+        String expectedMessage = getContext().getString(R.string.exception_message_user_not_found);
+        String actualMessage = ErrorMessageFactory.create(getContext(), new SongNotFoundException());
+        assertThat(actualMessage, is(equalTo(expectedMessage)));
+    }
+}
--- domain/src/test/java/com/pencorp/domain/exception/DefaultErrorBundleTest.java
+++ domain/src/test/java/com/pencorp/domain/exception/DefaultErrorBundleTest.java
@@ -1,8 +1,33 @@
 package com.pencorp.domain.exception;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Mockito.verify;
+
 /**
  * Created by Tuane on 15/02/17.
  */
 
 public class DefaultErrorBundleTest {
+
+    private DefaultErrorBundle defaultErrorBundle;
+
+    @Mock
+    private Exception mockException;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        defaultErrorBundle = new DefaultErrorBundle(mockException);
+    }
+
+    @Test
+    public void testGetErrorMessageInteraction() {
+        defaultErrorBundle.getErrorMessage();
+
+        verify(mockException).getMessage();
+    }
 }
