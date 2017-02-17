287d2541964902c77a6a3dd3304449869bdaeb63
Pentico <morulaneat@gmail.com>
2017-02-17 23:54:12 +0200

    Even More class

---
+++ Presentation/src/androidTest/java/com/pencorp/studiotam/mapper/SongModelDataMapperTest.java
@@ -0,0 +1,8 @@
+package com.pencorp.studiotam.mapper;
+
+/**
+ * Created by Tuane on 17/02/17.
+ */
+
+public class SongModelDataMapperTest {
+}
---
+++ Presentation/src/androidTest/java/com/pencorp/studiotam/presenter/SongListPresenterTest.java
@@ -0,0 +1,8 @@
+package com.pencorp.studiotam.presenter;
+
+/**
+ * Created by Tuane on 17/02/17.
+ */
+
+public class SongListPresenterTest {
+}
