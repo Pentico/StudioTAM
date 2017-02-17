c770de8aba8a9aa88e52e2acf9dc90719ff3c845
Pentico <morulaneat@gmail.com>
2017-02-18 00:39:26 +0200

    Need to run this test ...

--- Presentation/src/androidTest/java/com/pencorp/studiotam/view/activity/SongListActivityTest.java
+++ Presentation/src/androidTest/java/com/pencorp/studiotam/view/activity/SongListActivityTest.java
@@ -1,8 +1,40 @@
 package com.pencorp.studiotam.view.activity;
 
+import android.content.Intent;
+import android.test.ActivityInstrumentationTestCase;
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.pencorp.studiotam.View.activity.SongListActivity;
+
 /**
  * Created by Tuane on 17/02/17.
  */
 
-public class SongListActivityTest {
+public class SongListActivityTest extends
+        ActivityInstrumentationTestCase2<SongListActivity> {
+
+    private SongListActivity songListActivity;
+
+    public SongListActivityTest() {
+        super(SongListActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.setActivityIntent(createTargetIntent());
+        songListActivity = getActivity();
+    } // EOF
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    } // EOF
+
+
+    private Intent createTargetIntent() {
+        Intent intent =
+                SongListActivity.getCallingIntent(getInstrumentation().getTargetContext());
+        return intent;
+    }
 }
