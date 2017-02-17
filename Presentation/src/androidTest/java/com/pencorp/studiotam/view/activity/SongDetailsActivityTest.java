a17eb881613be5c6e2a18b1530e2e616be00e4bc
Pentico <morulaneat@gmail.com>
2017-02-18 00:25:04 +0200

    Need to run this test ...

--- Presentation/src/androidTest/java/com/pencorp/studiotam/view/activity/SongDetailsActivityTest.java
+++ Presentation/src/androidTest/java/com/pencorp/studiotam/view/activity/SongDetailsActivityTest.java
@@ -1,8 +1,25 @@
 package com.pencorp.studiotam.view.activity;
 
+import com.pencorp.studiotam.View.activity.SongDetailsActivity;
+
+
+import android.test.ActivityInstrumentationTestCase2;
+
+
+
 /**
  * Created by Tuane on 17/02/17.
  */
 
-public class SongDetailsActivityTest {
+public class SongDetailsActivityTest  extends
+        ActivityInstrumentationTestCase2<SongDetailsActivity> {
+
+    private static final long FAKE_SONG_ID = 264;
+    private static final String FAKE_SONG_PATH = "sdCard/music/";
+
+    private SongDetailsActivity songDetailsActivity;
+
+    public SongDetailsActivityTest() {
+        super(SongDetailsActivity.class);
+    }
 }
