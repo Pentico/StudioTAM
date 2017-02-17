f6a341b23d0423f9121811691b9391667101b1ab
Pentico <morulaneat@gmail.com>
2017-02-18 00:59:55 +0200

    Need to run this test ...

--- Presentation/src/androidTest/java/com/pencorp/studiotam/presenter/SongDetailsPresenterTest.java
+++ Presentation/src/androidTest/java/com/pencorp/studiotam/presenter/SongDetailsPresenterTest.java
@@ -1,8 +1,56 @@
 package com.pencorp.studiotam.presenter;
 
+import android.content.Context;
+import android.test.AndroidTestCase;
+
+import com.pencorp.domain.interactor.GetSong;
+import com.pencorp.studiotam.View.SongDetailsView;
+import com.pencorp.studiotam.mapper.SongModelDataMapper;
+
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import rx.Subscriber;
+
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
+
 /**
  * Created by Tuane on 17/02/17.
  */
 
-public class SongDetailsPresenterTest {
+public class SongDetailsPresenterTest extends AndroidTestCase {
+
+    private static final long FAKE_SONG_ID = 425;
+
+    private SongDetailsPresenter songDetailsPresenter;
+
+    @Mock
+    private Context mockContext;
+    @Mock
+    private SongDetailsView mockSongDetailsView;
+    @Mock
+    private GetSong mockGetSong;
+    @Mock
+    private SongModelDataMapper mockSongModelDataMapper;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        MockitoAnnotations.initMocks(this);
+        songDetailsPresenter = new SongDetailsPresenter(mockGetSong,
+                mockSongModelDataMapper);
+        songDetailsPresenter.setView(mockSongDetailsView);
+    }
+
+    public  void  testSongDetailsPresenterInitialze() {
+        given(mockSongDetailsView.context()).willReturn(mockContext);
+
+        songDetailsPresenter.initialize();
+
+        verify(mockSongDetailsView).hideRetry();
+        verify(mockSongDetailsView).showLoading();
+        verify(mockSongDetailsView).execute(any(Subscriber.class));
+    }
 }
